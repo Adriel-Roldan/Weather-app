361680320f0d4c46374f83c604ffd40482fd2faf
Adriel-Roldan <adrielemilioroldanchitadino@gmail.com>
2022-10-04 01:01:29 -0300

    App 80%

--- root/js/index.js
+++ root/js/index.js
@@ -1,6 +1,7 @@
 const apiKey = 'df32e3f7e75ec41184aea0d4145e123d';
 
 const fetchData = position => {
+
     const {latitude, longitude} = position.coords;
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${apiKey}`)
@@ -9,8 +10,12 @@ const fetchData = position => {
         .then(data => setWeaterData(data));
 }
 
+
+
 const setWeaterData = data => {
+
     console.log(data);
+
     const weatherData = {
         locationRegion: data.name,
         locationCountry: data.sys.country,
@@ -32,10 +37,12 @@ const setWeaterData = data => {
 const getDate = () => {
     
     let date = new Date();
+
     return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
 }
 
 const onLoad = () => {
+
     navigator.geolocation.getCurrentPosition(fetchData);
 }
 
